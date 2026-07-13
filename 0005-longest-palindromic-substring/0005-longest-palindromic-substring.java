47405b8a57bc90783636ce0a40abeb97bedc0e7b
harinikamana <155606398+harinikamana@users.noreply.github.com>
2026-07-13 09:43:32 +0530

    Time: 13 ms (96.26%), Space: 43.4 MB (89.42%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,26 +1,24 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-        String res = "";
-        for(int i = 0; i<s.length(); i++){
-            for(int j = i; j<s.length(); j++){
-                if(isPalindrome(s,i,j)){
-                    if((j-i+1)>res.length()){
-                        res = s.substring(i,j+1);
-                    }
-                }
+        int start = 0;
+        int end = 0;
+        for(int i = 0; i<n; i++){
+            int len1 = isPalindrome(s, i, i);
+            int len2 = isPalindrome(s, i, i+1);
+            int len = Math.max(len1, len2);
+            if(len>(end-start)){
+                start = i-(len-1)/2;
+                end = i+len/2;
             }
         }
-        return res;
+        return s.substring(start, end+1);
     }
-    private boolean isPalindrome(String s, int left, int right){
-        while(left<right){
-            if(s.charAt(left)!=s.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
+    private int isPalindrome(String s, int left, int right){
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
         }
-        return true;
+        return right-left-1;
     }
 }
